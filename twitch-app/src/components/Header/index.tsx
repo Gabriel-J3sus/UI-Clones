c78f8843da3468bfc591c6f653489735f92c445f
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-11-24 18:22:39 -0300

    Twitch clone

---
+++ twitch-app/src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+import { Container, Avatar, OnlineStatus, RightSide, Button } from './styles';
+import colors from '../../styles/colors';
+
+const Header: React.FC = () => {
+    return (
+        <Container>
+            <Avatar>
+                <OnlineStatus />
+            </Avatar>
+
+            <RightSide>
+                <Button>
+                    <MaterialIcons
+                        name="notifications-none"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+
+                <Button>
+                    <MaterialCommunityIcons
+                        name="message-outline"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+
+                <Button>
+                    <Feather
+                        name="search"
+                        size={26}
+                        color={colors.black}
+                    />
+                </Button>
+            </RightSide>
+        </Container>
+    );
+}
+
+export default Header;
